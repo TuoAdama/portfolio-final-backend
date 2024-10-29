400a5069da6fc592e4b99a0223d1186a477aec1e
TuoAdama <tuoadama17@gmail.com>
2024-10-29 21:59:13 +0100

    feat: remove used import

--- adama/src/main/java/com/portfolio/adama/PortfolioApplication.java
+++ adama/src/main/java/com/portfolio/adama/PortfolioApplication.java
@@ -2,9 +2,6 @@ package com.portfolio.adama;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PortfolioApplication {
